4ce2f8680c1374ad1a66a27cddea1d8e61ee4333
Graeme Stirling <graemejstirling@gmail.com>
2022-07-27 11:27:10 +0100

    First commit

---
+++ src/main/scala/ReceiptPrinter.scala
@@ -0,0 +1,28 @@
+/**
+ * This method should return a multiline string
+ * representing a ReceiptPrinter receipt that should show
+ * - the date and time the receipt was created
+ * - each item in the order, with the price. eg:
+ *     2 x Blueberry Muffin       8.10
+ *     1 x Cappuccino             3.85
+ * - the total price
+ * - the VAT (20% of total price)
+ */
+
+class CafeDetails (
+                    val shopName: String,
+                    val address: String,
+                    val phone: String,
+                    val prices: Map[String, Double]
+                  )
+
+class ReceiptPrinter(val cafe: CafeDetails, var order: Map[String, Int] = Map()) {
+
+  def receipt: String = {
+    s"${cafe.shopName}, ${cafe.address}, ${cafe.phone}"
+  }
+
+  def getDateTime(): Unit = {
+
+  }
+}
