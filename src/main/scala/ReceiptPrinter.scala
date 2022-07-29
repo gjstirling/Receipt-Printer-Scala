2a9f236155b943a2ead8ca4ca09135c7e47d5a8e
Graeme Stirling <graemejstirling@gmail.com>
2022-07-29 17:14:41 +0100

    Reseting main

--- src/main/scala/ReceiptPrinter.scala
+++ src/main/scala/ReceiptPrinter.scala
@@ -1,5 +1,4 @@
 import java.time.{Clock, Instant, ZoneId}
-import java.time.format.DateTimeFormatter
 
 class CafeDetails (
                     val shopName: String,
@@ -11,48 +10,7 @@ class CafeDetails (
 class ReceiptPrinter(val cafe: CafeDetails, var order: Map[String, Int] = Map(), val clock: Clock = Clock.systemUTC()) {
 
   def receipt: String = {
-    var orderDetails = stringifyOrder(order)
-    var totalPrice = orderTotal(order)
-    var date = getDateTime()
       s"""${cafe.shopName}, ${cafe.address}, ${cafe.phone}
-       |${date}
-       |${orderDetails}
-       |Total: ${totalPrice}
-       |VAT: ${vat(totalPrice)}
        |""".stripMargin
-  }
-
-  private[this] def getDateTime(): String = {
-    val timeNow = Instant.now(clock)
-    val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault())
-    val formattedDate = formatter.format(timeNow)
-    formattedDate
-  }
-
-  private[this] def stringifyOrder(order: Map[String, Int]): String = {
-    var stringOrder = order map {case (key, quantity) => (
-      s"${quantity} x ${key}     ${calculatePrice(cafe.prices(key), quantity)}"
-    )}
-    stringOrder.mkString("\n")
-  }
-
-  private[this] def calculatePrice(price: Double, quantity: Int): String ={
-    val formatter = java.text.NumberFormat.getCurrencyInstance
-    val calculatePrice = price*100*quantity/100
-    formatter.format(calculatePrice).substring(1)
-  }
-
-  private[this] def orderTotal(order: Map[String, Int]): Double ={
-    var itemsCost = order map {case (item, quantity) => (
-      cafe.prices(item)*quantity
-    )}
-    var result = itemsCost.reduce(_ + _)
-    var total = calculatePrice(result, 1)
-    total.toDouble
-  }
-
-  private[this] def vat(total: Double): String ={
-    var vat = total * 0.2
-    calculatePrice(vat, 1)
   }
 }
--- src/test/scala/ReceiptPrinterTest.scala
+++ src/test/scala/ReceiptPrinterTest.scala
@@ -35,63 +35,7 @@ class ReceiptPrinterSpec extends AnyWordSpec with Matchers {
         )
         printer.receipt should include ("The Coffee Connection, 123 Lakeside Way, 16503600708")
       }
-
-      "Contains the date/time it was created" in {
-        val printer = new ReceiptPrinter(
-          coffeeConnectionCafe,
-          Map("Cafe Latte" -> 1),
-          Clock.fixed(Instant.parse("2022-07-27T10:50:00.00Z"), ZoneId.systemDefault())
-        )
-        printer.receipt should include ("27/07/2022 11:50")
-      }
-
-      "Can return a single item ordered" in {
-        val order = Map(
-          "Flat White" -> 1
-        )
-        val printer = new ReceiptPrinter(
-          coffeeConnectionCafe, order
-        )
-        printer.receipt should include ("1 x Flat White     4.75")
-      }
-
-      "Can return a multiple different items" in {
-        val order = Map(
-          "Flat White" -> 2,
-          "Muffin Of The Day" -> 1
-        )
-        val printer = new ReceiptPrinter(
-          coffeeConnectionCafe, order
-        )
-        printer.receipt should include ("2 x Flat White     9.50")
-        printer.receipt should include ("1 x Muffin Of The Day     4.55")
-      }
-
-      "Can calculate the total cost of the order" in {
-        val order = Map(
-          "Flat White" -> 2,
-          "Muffin Of The Day" -> 1
-        )
-        val printer = new ReceiptPrinter(
-          coffeeConnectionCafe, order
-        )
-        printer.receipt should include ("2 x Flat White     9.50")
-        printer.receipt should include ("1 x Muffin Of The Day     4.55")
-        printer.receipt should include ("Total: 14.05")
-      }
-
-      "Can calculate and display the VAT" in {
-        val order = Map(
-          "Flat White" -> 2,
-          "Muffin Of The Day" -> 1
-        )
-        val printer = new ReceiptPrinter(
-          coffeeConnectionCafe, order
-        )
-        printer.receipt should include ("VAT: 2.81")
-      }
     }
-
   }
 }
 
